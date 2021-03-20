a2aa743145bcb14c453cfe100545cd454ca7f1d6
Minigrim0 <grimauflorent@gmail.com>
2021-03-20 17:48:14 +0100

    [Add] game class

---
+++ src/game.py
@@ -0,0 +1,33 @@
+import json
+
+from src.map import Map
+from src.player import Player
+
+
+class Game(object):
+    def __init__(self, saveFile=None):
+        self.isRunning = False
+        if saveFile is None:
+            self.map = Map("assets/maps/default.json")
+            self.player = Player()
+        else:
+            self.load(saveFile)
+
+    def load(self, saveFile):
+        """
+        Loads the game from a save file, creates an empty game if the save is None
+        """
+        save = json.loads(open(saveFile, "r"))
+        self.map = Map(save['map'])
+        self.player = Player(save['player'])
+
+    def update(self, screen):
+        pass
+
+    def draw(self, screen):
+        self.map.draw(screen, (0, 0))
+
+    def run(self, screen):
+        while self.isRunning:
+            self.update()
+            self.draw()
