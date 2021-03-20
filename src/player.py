ccfe39be3fad0a99981c7ed1a87b658f2fbbcbd3
Minigrim0 <grimauflorent@gmail.com>
2021-03-21 00:11:27 +0100

    [Add] HIT HIT HIT HIT

--- src/player.py
+++ src/player.py
@@ -1,5 +1,7 @@
 import pygame
 
+import src.constants as cst
+
 
 class Player(object):
     def __init__(self):
@@ -8,6 +10,9 @@ class Player(object):
         self.image = pygame.image.load("assets/img/player/player_idle_1.png").convert_alpha()
         self.velocity = [0, 0]
         self.speed = 400
+
+        self.status = cst.IDLE
+        self.damage = 30
 
         self.map_pos = 0
 
@@ -33,14 +38,28 @@ class Player(object):
         if pygame.key.get_pressed()[pygame.locals.K_RIGHT]:
             self.velocity[0] = 1
 
-        if self.position[0] < 500:
-            self.position[0] += self.speed * self.velocity[0] * screen.timeElapsed
-        elif self.velocity[0] < 0:
-            self.position[0] += self.speed * self.velocity[0] * screen.timeElapsed
-        else:
-            self.map_pos -= self.speed * self.velocity[0] * screen.timeElapsed
+        self.move(screen.timeElapsed)
 
-        self.position[1] += self.speed * self.velocity[1] * screen.timeElapsed
+    def move(self, timeElapsed):
+        if self.position[0] < 500:
+            self.position[0] += self.speed * self.velocity[0] * timeElapsed
+        elif self.velocity[0] < 0:
+            self.position[0] += self.speed * self.velocity[0] * timeElapsed
+        else:
+            self.map_pos -= self.speed * self.velocity[0] * timeElapsed
+
+        self.position[1] += self.speed * self.velocity[1] * timeElapsed
 
         self.velocity[0] -= self.velocity[0] * 0.1
         self.velocity[1] -= self.velocity[1] * 0.1
+
+    def eventUpdate(self, event):
+        if event.type == pygame.locals.KEYDOWN:
+            if event.key == pygame.locals.K_SPACE:
+                self.status = cst.ATTACK
+                print("HIT ME")
+                return "HIT"
+
+    @property
+    def hitbox(self):
+        return pygame.Rect((self.position[0] + cst.HITBOX_RELATIVE[0], self.position[1]), cst.HITBOX_RELATIVE[2:])
