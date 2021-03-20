a82549b70d1ba7365ead917d3d93ba70bcbd5230
Minigrim0 <grimauflorent@gmail.com>
2021-03-20 17:05:56 +0100

    [Upd]  main

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pygame
+from src.menu import Menu, Button
+from src.screen import Screen
+
 
 pygame.init()
 
+screen = Screen((1920, 1080), "Stop the count!", fullScreen=True)
+
+button_play = Button((100, 100), (250, 40), "Test lel", None)
+button_play.build(screen)
+mainMenu = Menu(pygame.image.load("assets/img/background.png"), [button_play])
+
+
 def main():
-    pass
+    mainMenu.run(screen)
+
 
 if __name__ == "__main__":
     main()
