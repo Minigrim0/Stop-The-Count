d7839d4b8df1633ab3908f5a24f2dad4a2f03ecd
Minigrim0 <grimauflorent@gmail.com>
2021-03-21 00:10:59 +0100

    [Upd] infinite map

--- src/map.py
+++ src/map.py
@@ -7,6 +7,15 @@ class Map(object):
     def __init__(self, mapFile):
         map_info = json.load(open(mapFile, "r"))
         self.image = pygame.image.load(map_info["img"]).convert_alpha()
+        self.size = self.image.get_size()
+        self.absolute_position = [0, 0]
 
-    def draw(self, screen, position):
+    def draw(self, screen, player):
+        position = [player.get_map_position(), 0]
+
         screen.blit(self.image, position)
+
+        if position[0] + self.size[0] < 0:
+            player.map_pos = position[0] + self.size[0]
+        elif self.size[0] - abs(position[0]) < screen.nativeSize[0]:
+            screen.blit(self.image, (self.size[0] - abs(position[0]), position[1]))
