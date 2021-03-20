00662eeeda100957f3c7eba4c38612948fcba56c
Minigrim0 <grimauflorent@gmail.com>
2021-03-20 17:47:45 +0100

    [Add] game in main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pygame
 from src.menu import Menu, Button
 from src.screen import Screen
+from src.game import Game
 
 
 pygame.init()
 
 screen = Screen((1920, 1080), "Stop the count!", fullScreen=False)
 
-button_play = Button((835, 200), (250, 40), "Jouer", None)
+game = Game()
+
+button_play = Button((835, 200), (250, 40), "Jouer", game.run, screen=screen)
 button_quit = Button((835, 300), (250, 40), "Quitter", exit)
 button_play.build(screen)
 button_quit.build(screen)
+
 mainMenu = Menu(pygame.image.load("assets/img/background.png"), [button_play, button_quit])
 
 
