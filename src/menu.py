f818f18d1e4c32efc7bd554d747a874351c6ba5b
Minigrim0 <grimauflorent@gmail.com>
2021-03-20 17:05:49 +0100

    [Upd] menu + add buttons

--- src/menu.py
+++ src/menu.py
@@ -1,3 +1,51 @@
+import pygame
+
+
 class Menu(object):
-    def __init__(self):
-        pass
+    def __init__(self, background, buttons=[]):
+        self.buttons = buttons
+        self.background = background
+        self.running = True
+
+    def update(self, screen):
+        for event in screen.events():
+            if event.type == pygame.locals.MOUSEBUTTONUP:
+                for button in self.buttons:
+                    button.click(event.pos)
+
+    def draw(self, screen):
+        screen.blit(self.background, (0, 0))
+        for button in self.buttons:
+            button.draw(screen)
+
+        screen.flip()
+
+    def run(self, screen):
+        while self.running:
+            self.update(screen)
+            self.draw(screen)
+
+
+class Button(object):
+    def __init__(self, pos, size, text, callback):
+        self.position = pos
+        self.size = size
+        self.text = text
+        self.callback = callback
+
+    def build(self, screen):
+        self.image = pygame.Surface((self.size))
+        self.image.blit(screen.fonts["25"].render(self.text, 1, (255, 255, 255)), (0, 0))
+
+    def draw(self, screen):
+        screen.blit(self.image, self.position)
+
+    def collide(self, position):
+        if self.position[0] + self.size[0] > position[0] > self.position[0]:
+            if self.position[1] + self.size[1] > position[1] > self.position[1]:
+                return True
+        return False
+
+    def click(self, position):
+        if self.collide(position):
+            self.callback()
