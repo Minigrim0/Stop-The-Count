bf3002cf2850a83b86c2ca81a2483d6e901cc530
yishyu <yishen.yu@ulb.be>
2021-03-21 10:21:00 +0100

    [Add] new KAMEHAMEHA

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,4 @@
+import pygame
 IDLE = 0
 ATTACK = 1
 WALK = 2
@@ -7,3 +8,10 @@ HURTBOX_RELATIVE = (0, 0, 150, 270)
 COLLISION_RELATIVE = (0, 270, 150, 150)
 
 ATTACK_COOLDOWN = 500  # Milliseconds
+WALK_COOLDOWN = 300
+
+LEFT_PUNCH = 0
+HIGH_KICK = 1
+LOW_KICK = 2
+
+ATTACK_KEYS = [pygame.locals.K_SPACE, pygame.locals.K_x, pygame.locals.K_c, pygame.locals.K_v]
