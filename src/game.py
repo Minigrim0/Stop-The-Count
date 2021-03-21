94707a4e92d7fe1629eed0cad643d54d6123b3f6
yishyu <yishen.yu@ulb.be>
2021-03-21 12:31:47 +0100

    [Del] save file option

--- src/game.py
+++ src/game.py
@@ -31,11 +31,10 @@ class Game(object):
         self.ennemyController = EnnemyController(5)
         button_help = Button((700, 300), (300, 60), "How to Build Walls", tutorialMenu.run, screen=screen)
         button_help.build(screen)
-        button_save = Button((1100, 300), (300, 60), "Save", exit)
-        button_save.build(screen)
+
         button_quit = Button((1500, 300), (300, 60), "Quit", self.stop)
         button_quit.build(screen)
-        self.pauseMenu = Menu(None, [button_help, button_save, button_quit], True)
+        self.pauseMenu = Menu(None, [button_help, button_quit], True)
 
         
         
