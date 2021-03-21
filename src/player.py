616e128fbc6438e009f50c2d0e804ae6bec460dc
Minigrim0 <grimauflorent@gmail.com>
2021-03-21 11:07:50 +0100

    [Add] Wall Building special attack

--- src/player.py
+++ src/player.py
@@ -11,7 +11,9 @@ class Player(object):
     def __init__(self):
         self.images = []
         self.position = [0, 600]
+        self.absolute_x = 0
         self.images = {}
+
         for filename in glob.glob("assets/img/player/animation/*.png"):
             dirname, file = os.path.split(filename)
             file, ext = os.path.splitext(file)
@@ -49,6 +51,8 @@ class Player(object):
 
         self.attackCooldown = 0
 
+        self.specialAttack = 0
+
         self.status = cst.IDLE
         self.damage = 30
 
@@ -65,7 +69,7 @@ class Player(object):
     def get_map_position(self):
         return self.map_pos
 
-    def update(self, screen):
+    def update(self, screen, ennemyController, invocations):
         if pygame.key.get_pressed()[pygame.locals.K_UP]:
             self.velocity[1] = -1
             if self.position[1] < screen.nativeSize[1] - 490 - self.size[1]:
@@ -81,33 +85,42 @@ class Player(object):
         if pygame.key.get_pressed()[pygame.locals.K_RIGHT]:
             self.velocity[0] = 1
 
-        self.move(screen.timeElapsed)
+        self.move(screen.timeElapsed, ennemyController, invocations)
+        self.specialAttack += screen.timeElapsed
 
         if self.attackCooldown > 0:
             self.attackCooldown -= screen.timeElapsed
             if self.attackCooldown <= 0:
                 self.status = cst.IDLE
 
-    def move(self, timeElapsed):
+    def move(self, timeElapsed, ennemyController, invocations):
         if self.position[0] < 500:
             self.position[0] += self.speed * self.velocity[0] * timeElapsed
         elif self.velocity[0] < 0:
             self.position[0] += self.speed * self.velocity[0] * timeElapsed
         else:
             self.map_pos -= self.speed * self.velocity[0] * timeElapsed
+            ennemyController.move(self.speed * self.velocity[0] * timeElapsed)
+            for invocation in invocations:
+                invocation.move(-self.speed * self.velocity[0] * timeElapsed)
 
         self.position[1] += self.speed * self.velocity[1] * timeElapsed
+        self.absolute_x += self.speed * self.velocity[0] * timeElapsed
 
         self.velocity[0] -= self.velocity[0] * 0.1
         self.velocity[1] -= self.velocity[1] * 0.1
 
     def eventUpdate(self, event):
+        print(self.specialAttack)
         if event.type == pygame.locals.KEYDOWN:
             if event.key == pygame.locals.K_SPACE and self.attackCooldown <= 0:
                 self.status = cst.ATTACK
                 self.attackCooldown = cst.ATTACK_COOLDOWN / 1000
                 pygame.mixer.Sound.play(random.choice(self.sounds["attack"]))
                 return "HIT"
+            elif event.key == pygame.locals.K_RETURN and self.status == cst.IDLE and self.specialAttack >= 25:
+                self.specialAttack = 0
+                return "BUILDWALL"
 
     @property
     def hitbox(self):
